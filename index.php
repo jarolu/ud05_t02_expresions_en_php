da2faad400479eefb073c9adfd5d68679af6e407
Luis Janeiro <jarolu@gmail.com>
2022-01-14 09:25:11 +0100

    Operadores aritméticos

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Construcións de expresións en PHP</title>
+</head>
+<body>
+    <h3>Construcións de expresións en PHP</h3>
+    <?php
+        // Operadores aritméticos
+        $x=10;
+        $y=3;
+        $z= $x * $y;
+        echo $x." x ".$y." = ".$z."<br>";
+        $z= $x % $y;
+        echo $x." módulo ".$y." = ".$z."<br>";
+
+
+
+    ?>
+</body>
+</html>
