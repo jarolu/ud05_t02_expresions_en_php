96212bc3ecdd8d659803145bd07204dd2e7386ec
Luis Janeiro <jarolu@gmail.com>
2022-01-17 09:30:41 +0100

    Operadores de incremento/diminución

--- index.php
+++ index.php
@@ -21,6 +21,11 @@
         echo "\$z-=\$y asigna a \$z ".$z."<br>";
         $z+=$y;
         echo "\$z+=\$y asigna a \$z ".$z."<br>";
+        //Operadores de incremento/diminución
+        ++$z;
+        echo "++\$z asigna a \$z ".$z."<br>";
+        $z--;
+        echo "\$z-- asigna a \$z ".$z."<br>";
 
 
 
